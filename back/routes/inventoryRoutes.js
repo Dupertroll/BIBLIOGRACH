5b719c8c7cd4be17c8477722ad3ec0f2c1aeae17
Dupertroll <laqm0103@gmail.com>
2025-07-05 23:51:25 -0500

    feat: implementar sistema de préstamos y mejorar organización del código
    - Agregado sistema de préstamos con nueva ruta /loan y controlador createPrestamo.
    - Implementada función loanService para crear préstamos en la base de datos.
    - Nueva función searchUserById para buscar usuarios por ID.
    - Nueva función searchBookId para buscar libros por nombre.
    - Refactorización de deleteService para usar searchBookId.
    - Mejoras en el formato y estructura del código.
    - Actualizado archivo de solicitudes HTTP con ejemplo de préstamo.

--- back/routes/inventoryRoutes.js
+++ back/routes/inventoryRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { createLibro, deleteLibro } from "../controllers/inventoryControler.js";
+import { createPrestamo } from "../controllers/loanControler.js";
 
 const router = express.Router();
 
 // Filtros de invenrario
 router.post("/create", createLibro);
 router.post("/delete", deleteLibro);
+router.post("/loan", createPrestamo);
 
 export default router;
